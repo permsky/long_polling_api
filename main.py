c9f2c1c0334c4da8a050236649565c7b8008c4c6
Vitaly Borodulin <rainbowf@mail.ru>
2022-06-09 21:59:19 +0500

    Fix notify function

--- main.py
+++ main.py
@@ -11,25 +11,24 @@ from loguru import logger
 def notify(
         token: str,
         chat_id: int,
-        response: dict) -> None:
+        user_reviews: dict) -> None:
     '''Send notification in telegram chat.'''
-    if response['status'] == 'found':
-        last_attempt = response['new_attempts'][0]
-        result = '''
-        Преподавателю все понравилось, можно приступать к следующему уроку!
-        '''
-        if last_attempt['is_negative']:
-            result = 'К сожалению, в работе нашлись ошибки.'
-        message_text = (
-            f'У вас проверили работу [{last_attempt["lesson_title"]}]'
-            f'({last_attempt["lesson_url"]})\n\n{result}'
-        )
-        bot = telegram.Bot(token)
-        bot.send_message(
-            chat_id=chat_id,
-            text=message_text,
-            parse_mode='Markdown'
-        )
+    last_attempt = user_reviews['new_attempts'][0]
+    result = '''
+    Преподавателю все понравилось, можно приступать к следующему уроку!
+    '''
+    if last_attempt['is_negative']:
+        result = 'К сожалению, в работе нашлись ошибки.'
+    message_text = (
+        f'У вас проверили работу [{last_attempt["lesson_title"]}]'
+        f'({last_attempt["lesson_url"]})\n\n{result}'
+    )
+    bot = telegram.Bot(token)
+    bot.send_message(
+        chat_id=chat_id,
+        text=message_text,
+        parse_mode='Markdown'
+    )
 
 
 @logger.catch
@@ -56,11 +55,12 @@ def main() -> None:
             )
             response.raise_for_status()
             user_reviews = response.json()
-            notify(
-                token=tg_token,
-                chat_id=chat_id,
-                response=user_reviews
-            )
+            if user_reviews['status'] == 'found':
+                notify(
+                    token=tg_token,
+                    chat_id=chat_id,
+                    user_reviews=user_reviews
+                )
             if user_reviews['status'] == 'timeout':
                 params = {'timestamp': user_reviews['timestamp_to_request']}
                 response = requests.get(
@@ -74,6 +74,7 @@ def main() -> None:
                     chat_id=chat_id,
                     response=response.json()
                 )
+                continue
         except requests.exceptions.ReadTimeout as err:
             logger.error(err)
         except ConnectionError as err:
